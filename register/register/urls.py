a772a9d949254aec9dd639a1c31ea25fa0e79402
kamran-01 <62847879+kamran-01@users.noreply.github.com>
2020-10-27 21:04:34 +0530

    added initial files

---
+++ register/register/urls.py
@@ -0,0 +1,32 @@
+"""
+register URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+# urls.py
+from django.contrib import admin
+from django.urls import path, include
+from register import views as v
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("register/", v.register, name="register"),
+    path('', include("main.urls")),
+    path('', include("django.contrib.auth.urls")),
+    path("home/", views.home, name="home"),
+    path("create/", views.create, name="create"),
+    path("view/", views.view, name="view"),
+]
