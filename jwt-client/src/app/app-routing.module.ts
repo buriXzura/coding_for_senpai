2310b60005acb3dabff0e0eac7ae8688bf823a74
Shabnam Sahay <shabnam@cse.iitb.ac.in>
2020-10-28 15:11:48 +0530

    readme and refs

--- jwt-client/src/app/app-routing.module.ts
+++ jwt-client/src/app/app-routing.module.ts
@@ -9,14 +9,13 @@ import { FileWorkComponent } from './file-work/file-work.component';
 import { AuthGuardService } from './auth-guard.service';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent },
   { path: 'profile', component: ProfileComponent, canActivate: [AuthGuardService]},
   { path: 'chngpswd', component: ChngpswdComponent, canActivate: [AuthGuardService]},
   { path: 'home', component: HomeComponent, canActivate: [AuthGuardService]},
   { path: 'filework', component: FileWorkComponent, canActivate: [AuthGuardService]},
-  { path: '**', redirectTo: '' }
+  { path: '**', component: LoginComponent }
 ];
 
 @NgModule({
