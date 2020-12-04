bbb739a874d327dccaed1937c98cfda0273a1d95
kamran-01 <62847879+kamran-01@users.noreply.github.com>
2020-12-05 00:15:24 +0530

    checkboxes

--- jwt-client/src/app/delete/delete.component.ts
+++ jwt-client/src/app/delete/delete.component.ts
@@ -2,11 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { FileService } from '../service/file.service';
 import {ThemePalette} from '@angular/material/core';
 
-export interface Task {
-  completed: boolean;
-  subtasks?: Task[];
-}
-
 @Component({
   selector: 'app-delete',
   templateUrl: './delete.component.html',
@@ -90,34 +85,5 @@ export class DeleteComponent implements OnInit {
     }
   }
 
-  // task: Task = {
-  //   completed: false,
-  //   subtasks: [
-  //     {completed: false},
-  //     {completed: false},
-  //     {completed: false}
-  //   ]
-  // };
-
-  // allComplete: boolean = false;
-
-  // updateAllComplete() {
-  //   this.allComplete = this.task.subtasks != null && this.task.subtasks.every(t => t.completed);
-  // }
-
-  // someComplete(): boolean {
-  //   if (this.task.subtasks == null) {
-  //     return false;
-  //   }
-  //   return this.task.subtasks.filter(t => t.completed).length > 0 && !this.allComplete;
-  // }
-
-  // setAll(completed: boolean) {
-  //   this.allComplete = completed;
-  //   if (this.task.subtasks == null) {
-  //     return;
-  //   }
-  //   this.task.subtasks.forEach(t => t.completed = completed);
-  // }
 
 }
