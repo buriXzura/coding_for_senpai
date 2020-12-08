fbeb3358df20062fd9ce7a63f22853ca6c8b0e4f
Shabnam Sahay <shabnam@cse.iitb.ac.in>
2020-12-08 09:48:08 +0530

    loginrevert + start README

--- jwt-client/src/app/login/login.component.ts
+++ jwt-client/src/app/login/login.component.ts
@@ -18,7 +18,7 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit() {
-    if(this.authService.authorized) {this.router.navigate(['/redir'])}
+    if(this.authService.authorized) {this.authService.logout();}
     this.form = this.fb.group({
       email: ['', Validators.email],
       password: ['', Validators.required],
